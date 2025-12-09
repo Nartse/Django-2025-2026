21b4f8d2680cc3cde67039156addc8b38d0ceba4
Nartse <tsenardi0@gmail.com>
2025-12-09 10:21:15 +0300

    Delete file

--- birth_year_calculator.py
+++
@@ -1,4 +0,0 @@
-num=int(input("Enter your age: "))
-year=int(input("What is the current year: "))
-birth=year-num
-print("Your birth year is: ", birth)
--- exception_read.py
+++
@@ -1,7 +0,0 @@
-try:
-    with open ("config.txt") as file:
-        content=file.read("config.txt")
-        file.read("Welcome, "+ content)
-except Exception:
-    with open ("guest.txt","w") as file:
-        file.write("Guest")
