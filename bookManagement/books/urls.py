56325236e3197453730e9993d9297bb50cde68e5
Nartse <tsenardi0@gmail.com>
2026-02-17 18:15:48 +0300

    books

--- bookManagement/books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('books/', BookListCreate.as_view()),
-    path('books/<int:pk>/', BookDetail.as_view()),
-    path('authors/', AuthorList.as_view()),
-    path('authors/<int:pk>/', AuthorDetail.as_view()),
-]
